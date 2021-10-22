2ab0c1ee71267406db6ab28cd4223a8b550944d5
Prutha <desaipruth0105@gmail.com>
2021-10-22 18:57:55 +0530

    Elibrary tables

--- Database ELibrary.sql
+++ Database ELibrary.sql
@@ -1,5 +1,32 @@
 ﻿--creating database for online library management system.
 
-CREATE DATABASE ELibrary;
 
---Create tables
+CREATE DATABASE ELibrary
+GO
+
+PRINT '"ELibrary" database is created.'
+
+USE ELibrary
+GO
+
+CREATE TABLE dbo.AuthorMaster
+(
+AuthorId	int		not null,
+AuthorName	varchar(20)		not null
+);
+
+PRINT '"Dbo.AuthorMaster" is created.'
+
+CREATE TABLE dbo.PublisherMaster
+(
+PublisherId	int	not null,
+PublisherName	varchar(20)	not null
+);
+
+PRINT '"Dbo.PublisherMaster" is created.'
+
+CREATE TABLE dbo.BookMaster
+(
+
+);
+PRINT '"Dbo.BookMaster" is created.'
